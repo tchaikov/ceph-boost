9f9b32539030a49e9b5813700f5cbd510d4939d2
Steve M. Robbins <smr@debian.org>
2003-09-21 01:35:19 +0000

    Imported upstream version 1.31.0.

--- boost/python/detail/signature.hpp
+++ boost/python/detail/signature.hpp
@@ -1,217 +1,89 @@
-//  (C) Copyright David Abrahams 2002. Permission to copy, use, modify, sell and
-//  distribute this software is granted provided this copyright notice appears
-//  in all copies. This software is provided "as is" without express or implied
-//  warranty, and with no claim as to its suitability for any purpose.
+#if !defined(BOOST_PP_IS_ITERATING)
+
+// Copyright David Abrahams 2002. Permission to copy, use,
+// modify, sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided
+// "as is" without express or implied warranty, and with no claim as
+// to its suitability for any purpose.
+
+# ifndef SIGNATURE_DWA20021121_HPP
+#  define SIGNATURE_DWA20021121_HPP
+
+#  include <boost/python/type_id.hpp>
+
+#  include <boost/python/detail/preprocessor.hpp>
+#  include <boost/python/detail/indirect_traits.hpp>
+
+#  include <boost/preprocessor/iterate.hpp>
+#  include <boost/preprocessor/iteration/local.hpp>
+
+#  include <boost/mpl/at.hpp>
+#  include <boost/mpl/size.hpp>
+
+namespace boost { namespace python { namespace detail { 
+
+struct signature_element
+{
+    char const* basename;
+    bool lvalue;
+};
+
+template <unsigned> struct signature_arity;
+
+#  define BOOST_PP_ITERATION_PARAMS_1                                            \
+        (3, (0, BOOST_PYTHON_MAX_ARITY + 1, <boost/python/detail/signature.hpp>))
+#  include BOOST_PP_ITERATE()
+
+// A metafunction returning the base class used for
 //
-//  This work was funded in part by Lawrence Berkeley and Lawrence
-//  Livermore National Labs
+//   signature<class F, class CallPolicies, class Sig>.
 //
-//  This file generated for 5-argument member functions and 6-argument free
-//  functions by gen_signature.py
-
-#ifndef SIGNATURE_DWA2002128_HPP
-# define SIGNATURE_DWA2002128_HPP
-
-# include <boost/mpl/type_list.hpp>
-
-namespace boost { namespace python { namespace detail {
-
-template <class R>
-mpl::type_list<R>
-signature(R (*)())
+template <class Sig>
+struct signature_base_select
 {
-    return mpl::type_list<R>();
-}
+    enum { arity = mpl::size<Sig>::value - 1 };
+    typedef typename signature_arity<arity>::template impl<Sig> type;
+};
 
-template <class R, class A0>
-mpl::type_list<R,A0>
-signature(R (*)(A0))
+template <class Sig>
+struct signature
+    : signature_base_select<Sig>::type
 {
-    return mpl::type_list<R,A0>();
-}
-
-template <class R, class A0, class A1>
-mpl::type_list<R,A0,A1>
-signature(R (*)(A0, A1))
-{
-    return mpl::type_list<R,A0,A1>();
-}
-
-template <class R, class A0, class A1, class A2>
-mpl::type_list<R,A0,A1,A2>
-signature(R (*)(A0, A1, A2))
-{
-    return mpl::type_list<R,A0,A1,A2>();
-}
-
-template <class R, class A0, class A1, class A2, class A3>
-mpl::type_list<R,A0,A1,A2,A3>
-signature(R (*)(A0, A1, A2, A3))
-{
-    return mpl::type_list<R,A0,A1,A2,A3>();
-}
-
-template <class R, class A0, class A1, class A2, class A3, class A4>
-mpl::type_list<R,A0,A1,A2,A3,A4>
-signature(R (*)(A0, A1, A2, A3, A4))
-{
-    return mpl::type_list<R,A0,A1,A2,A3,A4>();
-}
-
-template <class R, class A0, class A1, class A2, class A3, class A4, class A5>
-mpl::type_list<R,A0,A1,A2,A3,A4,A5>
-signature(R (*)(A0, A1, A2, A3, A4, A5))
-{
-    return mpl::type_list<R,A0,A1,A2,A3,A4,A5>();
-}
-template <class R, class A0>
-mpl::type_list<R,A0&> signature(R (A0::*)())
-{
-    return mpl::type_list<R,A0&>();
-}
-
-template <class R, class A0, class A1>
-mpl::type_list<R,A0&,A1> signature(R (A0::*)(A1))
-{
-    return mpl::type_list<R,A0&,A1>();
-}
-
-template <class R, class A0, class A1, class A2>
-mpl::type_list<R,A0&,A1,A2> signature(R (A0::*)(A1, A2))
-{
-    return mpl::type_list<R,A0&,A1,A2>();
-}
-
-template <class R, class A0, class A1, class A2, class A3>
-mpl::type_list<R,A0&,A1,A2,A3> signature(R (A0::*)(A1, A2, A3))
-{
-    return mpl::type_list<R,A0&,A1,A2,A3>();
-}
-
-template <class R, class A0, class A1, class A2, class A3, class A4>
-mpl::type_list<R,A0&,A1,A2,A3,A4> signature(R (A0::*)(A1, A2, A3, A4))
-{
-    return mpl::type_list<R,A0&,A1,A2,A3,A4>();
-}
-
-template <class R, class A0, class A1, class A2, class A3, class A4, class A5>
-mpl::type_list<R,A0&,A1,A2,A3,A4,A5> signature(R (A0::*)(A1, A2, A3, A4, A5))
-{
-    return mpl::type_list<R,A0&,A1,A2,A3,A4,A5>();
-}
-
-
-template <class R, class A0>
-mpl::type_list<R,A0 const&> signature(R (A0::*)() const)
-{
-    return mpl::type_list<R,A0 const&>();
-}
-
-template <class R, class A0, class A1>
-mpl::type_list<R,A0 const&,A1> signature(R (A0::*)(A1) const)
-{
-    return mpl::type_list<R,A0 const&,A1>();
-}
-
-template <class R, class A0, class A1, class A2>
-mpl::type_list<R,A0 const&,A1,A2> signature(R (A0::*)(A1, A2) const)
-{
-    return mpl::type_list<R,A0 const&,A1,A2>();
-}
-
-template <class R, class A0, class A1, class A2, class A3>
-mpl::type_list<R,A0 const&,A1,A2,A3> signature(R (A0::*)(A1, A2, A3) const)
-{
-    return mpl::type_list<R,A0 const&,A1,A2,A3>();
-}
-
-template <class R, class A0, class A1, class A2, class A3, class A4>
-mpl::type_list<R,A0 const&,A1,A2,A3,A4> signature(R (A0::*)(A1, A2, A3, A4) const)
-{
-    return mpl::type_list<R,A0 const&,A1,A2,A3,A4>();
-}
-
-template <class R, class A0, class A1, class A2, class A3, class A4, class A5>
-mpl::type_list<R,A0 const&,A1,A2,A3,A4,A5> signature(R (A0::*)(A1, A2, A3, A4, A5) const)
-{
-    return mpl::type_list<R,A0 const&,A1,A2,A3,A4,A5>();
-}
-
-
-template <class R, class A0>
-mpl::type_list<R,A0 volatile&> signature(R (A0::*)() volatile)
-{
-    return mpl::type_list<R,A0 volatile&>();
-}
-
-template <class R, class A0, class A1>
-mpl::type_list<R,A0 volatile&,A1> signature(R (A0::*)(A1) volatile)
-{
-    return mpl::type_list<R,A0 volatile&,A1>();
-}
-
-template <class R, class A0, class A1, class A2>
-mpl::type_list<R,A0 volatile&,A1,A2> signature(R (A0::*)(A1, A2) volatile)
-{
-    return mpl::type_list<R,A0 volatile&,A1,A2>();
-}
-
-template <class R, class A0, class A1, class A2, class A3>
-mpl::type_list<R,A0 volatile&,A1,A2,A3> signature(R (A0::*)(A1, A2, A3) volatile)
-{
-    return mpl::type_list<R,A0 volatile&,A1,A2,A3>();
-}
-
-template <class R, class A0, class A1, class A2, class A3, class A4>
-mpl::type_list<R,A0 volatile&,A1,A2,A3,A4> signature(R (A0::*)(A1, A2, A3, A4) volatile)
-{
-    return mpl::type_list<R,A0 volatile&,A1,A2,A3,A4>();
-}
-
-template <class R, class A0, class A1, class A2, class A3, class A4, class A5>
-mpl::type_list<R,A0 volatile&,A1,A2,A3,A4,A5> signature(R (A0::*)(A1, A2, A3, A4, A5) volatile)
-{
-    return mpl::type_list<R,A0 volatile&,A1,A2,A3,A4,A5>();
-}
-
-
-template <class R, class A0>
-mpl::type_list<R,A0 const volatile&> signature(R (A0::*)() const volatile)
-{
-    return mpl::type_list<R,A0 const volatile&>();
-}
-
-template <class R, class A0, class A1>
-mpl::type_list<R,A0 const volatile&,A1> signature(R (A0::*)(A1) const volatile)
-{
-    return mpl::type_list<R,A0 const volatile&,A1>();
-}
-
-template <class R, class A0, class A1, class A2>
-mpl::type_list<R,A0 const volatile&,A1,A2> signature(R (A0::*)(A1, A2) const volatile)
-{
-    return mpl::type_list<R,A0 const volatile&,A1,A2>();
-}
-
-template <class R, class A0, class A1, class A2, class A3>
-mpl::type_list<R,A0 const volatile&,A1,A2,A3> signature(R (A0::*)(A1, A2, A3) const volatile)
-{
-    return mpl::type_list<R,A0 const volatile&,A1,A2,A3>();
-}
-
-template <class R, class A0, class A1, class A2, class A3, class A4>
-mpl::type_list<R,A0 const volatile&,A1,A2,A3,A4> signature(R (A0::*)(A1, A2, A3, A4) const volatile)
-{
-    return mpl::type_list<R,A0 const volatile&,A1,A2,A3,A4>();
-}
-
-template <class R, class A0, class A1, class A2, class A3, class A4, class A5>
-mpl::type_list<R,A0 const volatile&,A1,A2,A3,A4,A5> signature(R (A0::*)(A1, A2, A3, A4, A5) const volatile)
-{
-    return mpl::type_list<R,A0 const volatile&,A1,A2,A3,A4,A5>();
-}
+};
 
 }}} // namespace boost::python::detail
 
-#endif // SIGNATURE_DWA2002128_HPP
+# endif // SIGNATURE_DWA20021121_HPP
+
+#else
+
+# define N BOOST_PP_ITERATION()
+
+template <>
+struct signature_arity<N>
+{
+    template <class Sig>
+    struct impl
+    {
+        static signature_element const* elements()
+        {
+            static signature_element const result[N+2] = {
+                
+# define BOOST_PP_LOCAL_MACRO(i)                                                            \
+     {                                                                                      \
+         type_id<BOOST_DEDUCED_TYPENAME mpl::at_c<Sig,i>::type>().name()                    \
+       , is_reference_to_non_const<BOOST_DEDUCED_TYPENAME mpl::at_c<Sig,i>::type>::value    \
+     },
+                
+# define BOOST_PP_LOCAL_LIMITS (0, N)
+# include BOOST_PP_LOCAL_ITERATE()
+                {0,0}
+            };
+            return result;
+        }
+    };
+};
+
+#endif // BOOST_PP_IS_ITERATING 
+
 
