aca097068c04ccc44b14632083d937f257a6fa10
Domenico Andreoli <cavok@debian.org>
2007-04-27 23:13:51 +0000

    successfully merged with upstream 1.34.0-beta1

--- boost/none.hpp
+++ boost/none.hpp
@@ -1,4 +1,6 @@
 // Copyright (C) 2003, Fernando Luis Cacciola Carballal.
+// Copyright (C) 2007, Anthony Williams
+// Copyright (C) 2007, Steven Watanabe, Richard Smith
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
@@ -7,17 +9,35 @@
 // See http://www.boost.org/lib/optional/ for documentation.
 //
 // You are welcome to contact the author at:
-//  fernando_cacciola@hotmail.com
+// fernando.cacciola@gmail.com
 //
 #ifndef BOOST_NONE_17SEP2003_HPP
 #define BOOST_NONE_17SEP2003_HPP
 
-namespace boost {
+namespace boost
+{
+  namespace detail
+  {
+    class none_helper;
+  }
 
-enum none_t {none = 0};
+  inline void none(detail::none_helper);
 
-} // namespace boost
+  namespace detail
+  {
+    class none_helper
+    {
+    private:
+      
+      none_helper( none_helper const& ) {}
+      
+      friend void boost::none(none_helper);
+    };
+  }
 
+  typedef void (*none_t)(detail::none_helper);
+
+  inline void none(detail::none_helper) {}
+}
 
 #endif
-
