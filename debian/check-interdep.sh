cf657642909e6658bc3642e0cc6c05159de3f86d
Steve M. Robbins <smr@debian.org>
2008-05-17 08:51:17 +0000

    Add missing dependencies.

---
+++ debian/check-interdep.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+libs='date_time 
+filesystem 
+graph 
+iostreams 
+program_options 
+python 
+regex 
+serialization
+signals
+system
+test
+thread
+wave'
+
+for l in $libs; do
+    for f in $libs; do
+	[ $l = $f ] && continue
+	rgrep -q boost/$f /usr/include/boost/$l && echo $l depends on $f
+    done
+done
