31e087a55e17e54717e4a8802914df26637ecd37
Steve M. Robbins <smr@debian.org>
2013-12-23 05:11:39 +0000

    Fix version numbers of libboost-coroutine1.54-dev & libboost-log1.54-dev.

--- debian/update-control.py
+++ debian/update-control.py
@@ -74,7 +74,7 @@ def printParagraph(p):
 
 def processControl():
     firstParagraph = True
-    for paragraph in Deb822.iter_paragraphs(open('control')):
+    for paragraph in Deb822.iter_paragraphs(open('control.in')):
         if firstParagraph:
             processSourceParagraph(paragraph)
             printParagraph(paragraph)
@@ -86,6 +86,6 @@ def processControl():
 
 
 
-gOldVersion = BoostVersion('1.53.0')
-gNewVersion = BoostVersion('1.54.0')
+gOldVersion = BoostVersion('1.54.0')
+gNewVersion = BoostVersion('1.55.0')
 processControl()
