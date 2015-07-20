699e9636fe2c449367249aecac43999ecfcce4f9
Dimitri John Ledkov <xnox@debian.org>
2015-07-20 23:02:46 +0000

    * New upstream release. * d/p/hurd-clock-gettime.patch: Remove, applied upstream. * Update d/documentation-files. * Don't call the compiler with -m{32,64} explicitly. * Build using ICU 55.1 * Include 1.58 known issues patches.

--- debian/update-control.py
+++ debian/update-control.py
@@ -86,6 +86,6 @@ def processControl():
 
 
 
-gOldVersion = BoostVersion('1.55.0')
-gNewVersion = BoostVersion('1.57.0')
+gOldVersion = BoostVersion('1.57.0')
+gNewVersion = BoostVersion('1.58.0')
 processControl()
