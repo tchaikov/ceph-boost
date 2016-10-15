5a5fc55f7de3ce9e01ef0ae1c5fbc868fe50927e
Steve M. Robbins <smr@debian.org>
2016-10-15 17:59:50 +0000

    Update control file for 1.62.

--- debian/update-control.py
+++ debian/update-control.py
@@ -86,6 +86,6 @@ def processControl():
 
 
 
-gOldVersion = BoostVersion('1.60.0')
-gNewVersion = BoostVersion('1.61.0')
+gOldVersion = BoostVersion('1.61.0')
+gNewVersion = BoostVersion('1.62.0')
 processControl()
