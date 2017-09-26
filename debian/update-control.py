269caad2f1454e1545d18468eeea2ab69f8089bb
Steve M. Robbins <smr@debian.org>
2017-09-26 01:37:29 +0000

    Update control for new upstream 1.65.1.

--- debian/update-control.py
+++ debian/update-control.py
@@ -86,6 +86,6 @@ def processControl():
 
 
 
-gOldVersion = BoostVersion('1.62.0')
-gNewVersion = BoostVersion('1.63.0')
+gOldVersion = BoostVersion('1.63.0')
+gNewVersion = BoostVersion('1.65.1')
 processControl()
